60801287ef8df40be767d54d0703350b2c462abe
Nazanin Azar <dev.nazaninazar@gmail.com>
2024-09-15 18:48:24 +0330

    Add App component: main structure and logic for managing items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Logo } from "./components/Logo";
+import { AddItemForm } from "./components/AddItemForm";
+import { ShoppingList } from "./components/ShoppingList";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItems(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, purchased: !item.purchased } : item
+      )
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <AddItemForm onAddItems={handleAddItems} />
+      <ShoppingList
+        items={items}
+        onDeleteItems={handleDeleteItem}
+        onToggleItems={handleToggleItems}
+      />
     </div>
   );
 }
